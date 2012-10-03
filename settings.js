667d06cbde86ac389f135782dd64cd90d09a845b
Rory Madden <rorymadden@gmail.com>
2012-10-03 10:43:32 -0300

    updated error handling to include error - based on express error example

--- settings.js
+++ settings.js
@@ -85,27 +85,52 @@ function bootApplication(app, config, passport) {
     // routes should be at the last
     app.use(app.router)
 
-    // assume "not found" in the error msgs
-    // is a 404. this is somewhat silly, but
-    // valid, you can do whatever you like, set
-    // properties, use instanceof etc.
-    app.use(function(err, req, res, next){
-      // treat as 404
-      if (~err.message.indexOf('not found')) return next()
+    // Since this is the last non-error-handling
+    // middleware use()d, we assume 404, as nothing else
+    // responded.
 
-      // log it
-      console.error(err.stack)
+    // $ curl http://localhost:3000/notfound
+    // $ curl http://localhost:3000/notfound -H "Accept: application/json"
+    // $ curl http://localhost:3000/notfound -H "Accept: text/plain"
 
-      // error page
-      res.status(500).render('500')
-    })
-
-    // assume 404 since no middleware responded
     app.use(function(req, res, next){
-      res.status(404).render('404', { url: req.originalUrl })
-    })
+      res.status(404);
+      
+      // respond with html page
+      if (req.accepts('html')) {
+        res.render('404', { url: req.url });
+        return;
+      }
 
-  })
+      // respond with json
+      if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+      }
+
+      // default to plain-text. send()
+      res.type('txt').send('Not found');
+    });
+
+    // error-handling middleware, take the same form
+    // as regular middleware, however they require an
+    // arity of 4, aka the signature (err, req, res, next).
+    // when connect has an error, it will invoke ONLY error-handling
+    // middleware.
+
+    // If we were to next() here any remaining non-error-handling
+    // middleware would then be executed, or if we next(err) to
+    // continue passing the error, only error-handling middleware
+    // would remain being executed, however here
+    // we simply respond with an error page.
+
+    app.use(function(err, req, res, next){
+      // we may use properties of the error object
+      // here and next(err) appropriately, or if
+      // we possibly recovered from the error, simply next().
+      res.status(err.status || 500);
+      res.render('500', { error: err });
+    });
 
   app.set('showStackError', false)
 
